f264b48c23d326ed06fd92e81a4682a222f24f7f
Aditya Pant <pantanupam05@gmail.com>
2025-02-20 22:00:30 +0530

    check

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
+import React, { useState, useEffect } from "react";
 import Todo from "./components";
+import { Employee } from "./components/person";
+
 function App() {
 	const userName = {
 		todo: {
@@ -8,9 +11,18 @@ function App() {
 			age: 19,
 		},
 	};
+	const emp1 = new Employee("aditya", 25);
+	const [greeting, setGreeting] = useState("");
+
+	useEffect(() => {
+		const message = `hello my name is ${emp1.name}`;
+		setGreeting(message);
+	}, []);
 	return (
 		<div>
 			<Todo todo={userName.todo} />
+			<h1>{greeting}</h1>
+			<h1>{emp1.age}</h1>
 		</div>
 	);
 }
---
+++ src/components/person.tsx
@@ -0,0 +1,16 @@
+interface Person {
+	name: string;
+	age: number;
+	greet(phrase: string): void;
+}
+export class Employee implements Person {
+	name: string;
+	age: number;
+	constructor(n: string, a: number) {
+		this.name = n;
+		this.age = a;
+	}
+	greet(phrase: string) {
+		console.log(`${phrase} ${this.name}`);
+	}
+}
